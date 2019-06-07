7a1b4e649a38bdef6a16650bb5a96a1662570e2a
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-06-07 10:26:43 +0100

    Ex3

--- HowManyOfThisType/Program.cs
+++ HowManyOfThisType/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections;
 
 namespace HowManyOfThisType
 {
     class Program
     {
+        // Exercicio 3
+        private static int MyNumbers<T>(IEnumerable numbers)
+        {
+            int total = 0;
+
+            foreach (object i in numbers)
+            {
+                if (i is T) total++;
+            }
+            return total;  
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            object[] myNumber = {"Ola", 3, 2.4f, "Dia", 10.2, 7, 12 };
+
+            Console.WriteLine($"N strings: {MyNumbers<string>(myNumber)}");
+            Console.WriteLine($"N ints: {MyNumbers<int>(myNumber)}");
         }
     }
 }
