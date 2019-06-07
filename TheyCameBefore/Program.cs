13e3a8d5c0086055c6ddd7868a22eafd18cc2509
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-06-07 11:20:17 +0100

    ex4

---
+++ TheyCameBefore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TheyCameBefore
+{
+    class Program
+    {
+        // Ex 4
+        private static IEnumerable<E> GetBefore<E>(IEnumerable <E> stuff, E x)
+            where E : IComparable<E>
+        {
+            foreach (E y in stuff)
+            {
+                if (y.CompareTo(x) < 0) yield return y;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] myInts = {2, 3, 5, 7, 8, -10, -6 };
+            foreach (int i in GetBefore<int>(myInts, 0))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
